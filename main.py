e7794339ce28aaed2318706be300263b65b01df2
salvador <salvador.cmenendez@gmail.com>
2019-12-06 22:55:52 -0600

    solve request using Set model

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import json
 from flask import escape
 from local.model import SetModel
 
 def processRequest(request):
     request_json = request.get_json(silent=True)
-    return request_json
+    request_args = request.args
+
+    if request_json and 'deck' in request_json:
+        deck = request_json['deck']
+    elif request_args and 'deck' in request_args:
+        deck = request_args['deck']
+    else:
+        return json.dumps({"error":"Missing 'dec' key"})
+    deck = json.loads(deck)
+    solution = SetModel.SolveFromRequest(deck)
+    return json.dumps(solution)
